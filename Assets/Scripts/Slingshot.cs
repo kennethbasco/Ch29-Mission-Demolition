8afab0bef7638eeaf0b03ce1d667df0d4d163d74
kennethbasco <kpb82@scarletmail.rutgers.edu>
2018-06-15 21:17:43 -0400

    Made Clouds

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -122,6 +122,8 @@ public class Slingshot : MonoBehaviour
 
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
 
+            FollowCam.POI = projectile;
+
             projectile = null;
 
         }
