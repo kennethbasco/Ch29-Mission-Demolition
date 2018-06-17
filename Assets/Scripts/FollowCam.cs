f2ded52dc5b0dc45c56fc7d5922fdb13faf77cc0
kennethbasco <kpb82@scarletmail.rutgers.edu>
2018-06-16 20:47:03 -0400

    Projectile Line

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -34,14 +34,55 @@ public class FollowCam : MonoBehaviour {
 
         // if there's only one line following an if, it doesn't need braces
 
-        if (POI == null) return; // return if there is no poi                   
+        // if (POI == null) return; // return if there is no poi                   
 
 
 
         // Get the position of the poi
 
 
-        Vector3 destination = POI.transform.position;
+        //Vector3 destination = POI.transform.position;
+
+        Vector3 destination;
+
+        // If there is no poi, return to P:[ 0, 0, 0 ]
+
+        if (POI == null)
+        {
+
+            destination = Vector3.zero;
+
+        }
+        else
+        {
+
+            // Get the position of the poi
+
+            destination = POI.transform.position;
+
+            // If poi is a Projectile, check to see if it's at rest
+
+            if (POI.tag == "Projectile")
+            {
+
+                // if it is sleeping (that is, not moving)
+
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+
+                    // return to default view
+
+                    POI = null;
+
+                    // in the next update
+
+                    return;
+
+                }
+
+            }
+
+        }
 
         // Limit the X & Y to minimum values
 
--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Slingshot : MonoBehaviour
 {
 
+    static private Slingshot S;
+
     [Header("Set in Inspector")]
 
     public GameObject prefabProjectile;
@@ -18,9 +20,25 @@ public class Slingshot : MonoBehaviour
     public bool aimingMode;
     private Rigidbody projectileRigidbody;
 
+    static public Vector3 LAUNCH_POS
+    {                                        
+
+        get
+        {
+
+            if (S == null) return Vector3.zero;
+
+            return S.launchPos;
+
+        }
+
+    }
+
 
     void Awake()
     {
+
+        S = this;
 
         Transform launchPointTrans = transform.Find("LaunchPoint");              
 
